2c218d6ffa0644fc21314f2c140ef466821dbbe8
vincentlee2004 <165283616+vincentlee2004@users.noreply.github.com>
2024-04-27 10:27:00 +0700

    Update main.dart dengan tampilan awal adalah Login

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:tugas/screens/home.dart';
+import 'package:tugas/components/login.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Home(),
+      home: Login(),
     );
   }
 }
