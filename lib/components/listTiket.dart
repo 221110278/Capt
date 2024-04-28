834170b9deaf3cf1d5cba2685717206a3391f9d6
JohnAlexanderSalim <165993587+JohnAlexanderSalim@users.noreply.github.com>
2024-04-28 19:47:29 +0800

    menambah checkbox untuk seat di bisokop

--- lib/components/listTiket.dart
+++ lib/components/listTiket.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:tugas/components/listUsers.dart';
-import 'package:tugas/data/listFilm.dart';
-import 'package:tugas/screens/home.dart';
+import 'package:proyek/components/listUsers.dart';
+import 'package:proyek/screens/home.dart';
 
 class ListTiket extends StatefulWidget {
   final int jumlahTiket;
@@ -13,71 +12,108 @@ class ListTiket extends StatefulWidget {
 }
 
 class _ListTiketState extends State<ListTiket> {
+  late List<bool> isSelected;
+  late int maxSelection;
+  late double cmToPixel;
+  late List<int> selectedSeats = [];
+
+  @override
+  void initState() {
+    super.initState();
+    isSelected = List<bool>.filled(16, false);
+    maxSelection = widget.jumlahTiket;
+  }
+
   @override
   Widget build(BuildContext context) {
-    final List<Map<String, dynamic>> filteredFilms = listFilm.where((film) => film['jumlahTiket'] > 0).toList();
+    cmToPixel = MediaQuery.of(context).devicePixelRatio * 1.7952755906;
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text(
-          'List Tiket',
-          style: TextStyle(
-            fontSize: 25,
-            fontWeight: FontWeight.bold,
-            color: Colors.white,
-          ),
-        ),
+        title: const Text('List Tiket'),
         backgroundColor: Colors.blue,
         centerTitle: true,
       ),
       body: Column(
         children: [
+          Container(
+            width: double.infinity,
+            height: 50,
+            color: Colors.grey[800],
+            alignment: Alignment.center,
+            child: const Text(
+              "Screen",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white),
+            ),
+          ),
           Expanded(
-            child: ListView.builder(
-              itemCount: filteredFilms.length,
+            child: GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 4,
+                childAspectRatio: 1.0,
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+              ),
+              itemCount: 16,
               itemBuilder: (context, index) {
-                String namaFilm = filteredFilms[index]["nama"];
-                int jumlahTiket = filteredFilms[index]["jumlahTiket"];
-                return ListTile(
-                  title: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(namaFilm),
-                      Text('$jumlahTiket tiket'),
-                    ],
-                  ),
-                  trailing: IconButton(
-                    onPressed: widget.jumlahTiket > 0 ? null : () {
-                      String username = listUsers.isNotEmpty ? listUsers.first["username"] : "username";
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => Home(username: username), 
+                return GestureDetector(
+                  onTap: () => _handleTap(index),
+                  child: SizedBox(
+                    width: cmToPixel, 
+                    height: cmToPixel,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: isSelected[index] ? Colors.blue : Colors.grey[300],
+                        border: Border.all(color: Colors.black),
+                      ),
+                      child: Center(
+                        child: Text(
+                          'Seat ${index + 1}',
+                          style: TextStyle(color: isSelected[index] ? Colors.white : Colors.black),
                         ),
-                      );
-                    },
-                    icon: Icon(widget.jumlahTiket > 0 ? Icons.check : Icons.local_movies),
+                      ),
+                    ),
                   ),
                 );
               },
             ),
           ),
           ElevatedButton(
+            onPressed: _orderSeats,
+            child: const Text('Pesan'),
+          ),
+          ElevatedButton(
             onPressed: () {
-              String username = listUsers.isNotEmpty ? listUsers.first["username"] : "username";
               Navigator.push(
                 context,
                 MaterialPageRoute(
-                  builder: (context) => Home(username: username),
+                  builder: (context) => Home(
+                    username: listUsers.isNotEmpty ? listUsers.first["username"] : "username",
+                    selectedSeats: selectedSeats, 
+                  ),
                 ),
               );
             },
-            child: Text('Kembali ke Menu Utama'),
+            child: const Text('Kembali ke Menu Utama'),
           ),
         ],
       ),
     );
   }
+
+  void _handleTap(int index) {
+    setState(() {
+      if (isSelected[index]) {
+        isSelected[index] = false;
+      } else {
+        if (isSelected.where((element) => element).length < maxSelection) {
+          isSelected[index] = true;
+        }
+      }
+    });
+  }
+
+  void _orderSeats() {
+    selectedSeats = List<int>.generate(isSelected.length, (i) => i).where((i) => isSelected[i]).toList();
+  }
 }
-
-
