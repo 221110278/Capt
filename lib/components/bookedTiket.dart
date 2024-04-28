b52f6ba82c509d544d0a811b941dfa9a8b517260
JohnAlexanderSalim <165993587+JohnAlexanderSalim@users.noreply.github.com>
2024-04-28 18:49:56 +0700

    pemesanan tiket

---
+++ lib/components/bookedTiket.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:proyek/components/listUsers.dart';
+import 'package:proyek/screens/home.dart';
+
+class BookedSeatsScreen extends StatelessWidget {
+  final List<int> bookedSeats;
+
+  const BookedSeatsScreen({Key? key, required this.bookedSeats}) : super(key: key);
+
+  @override
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(
+      title: Text('Booked Seats'),
+    ),
+    body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Booked Seats:',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 20),
+          Text(
+            bookedSeats.isEmpty ? 'No seats booked' : bookedSeats.join(', '),
+            style: TextStyle(fontSize: 16),
+          ),
+          SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => Home(username: listUsers.isNotEmpty ? listUsers.first["username"] : "username",selectedSeats: [],),
+                ),
+              ); // Navigate back to the previous screen (Home screen)
+            },
+            child: Text('Return to Home'),
+          ),
+        ],
+      ),
+    ),
+  );
+}
+}
