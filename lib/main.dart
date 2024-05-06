e9f11246041e1ddbdf241561ca2657bf787a7eb0
221110278 <165923810+221110278@users.noreply.github.com>
2024-05-07 02:11:52 +0700

    update fitur theme provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:tugas/components/login.dart';
+import 'package:tugas/components/prov_set.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MultiProvider(providers: [
+    ChangeNotifierProvider(create: (_)=> SettingsProvider()),
+  ],
+  child: const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    final prov = Provider.of<SettingsProvider>(context);
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: prov.enableDarkMode == true ? prov.dark : prov.light,
       home: Login(),
     );
   }
