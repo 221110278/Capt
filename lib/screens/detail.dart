f49957e5b91aa1904104f1db1f7159994ed4abf4
221110278 <165923810+221110278@users.noreply.github.com>
2024-05-07 02:14:32 +0700

    menambahkan floating action button favorite

--- lib/screens/detail.dart
+++ lib/screens/detail.dart
@@ -2,15 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:tugas/components/pesan-tiket.dart';
 import 'package:tugas/data/listFilm.dart';
 
-class Detail extends StatelessWidget {
-  Detail({Key? key, required this.nama});
+class Detail extends StatefulWidget {
+  const Detail({Key? key, required this.nama,});
 
   final String nama;
+  @override
+  State<Detail> createState() => _DetailState();
+}
 
+class _DetailState extends State<Detail> {
+  bool isFavorite = false;
   @override
   Widget build(BuildContext context) {
-  Map<String, dynamic> selectedFilm = listFilm.firstWhere((film) => film['nama'] == nama);    
-
+  Map<String, dynamic> selectedFilm = listFilm.firstWhere((film) => film['nama'] == widget.nama);
     return Scaffold(
       appBar: AppBar(
         title: Text(selectedFilm['nama']),
@@ -38,13 +42,13 @@ class Detail extends StatelessWidget {
                 Image.network(
                   selectedFilm['gambar'],
                 ),
-                SizedBox(height: 20),
+                const SizedBox(height: 20),
                 Text(
                   '${selectedFilm['nama']}',
-                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                  style: const TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
                 ),
-                SizedBox(height: 10),
-                Row(
+                const SizedBox(height: 10),
+                const Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                   Icon(
@@ -73,31 +77,41 @@ class Detail extends StatelessWidget {
                   size: 24,
                 ),
                 ],),
-                SizedBox(height: 10),
+                const SizedBox(height: 10),
                 Center(
-                  child: Container(
+                  child: SizedBox(
                     width: 500,
                     child: Text(
                       '${selectedFilm['deskripsi']}',
-                      style: TextStyle(fontSize: 16),textAlign: TextAlign.justify,
+                      style: const TextStyle(fontSize: 16),textAlign: TextAlign.justify,
                     ),
                   ),
                 ),
-               SizedBox(height: 20),
+               const SizedBox(height: 20),
                 ElevatedButton(
                   onPressed: () {
                     Navigator.push(context, MaterialPageRoute(
                       builder: (context) {
-                      return Tiket(nama: nama);
+                      return Tiket(nama: widget.nama);
                   }));
                   },
-                  child: Text('Pesan Tiket'),
+                  child: const Text('Pesan Tiket'),
                 ),
               ],
             ),
           ),
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          setState(() {
+            isFavorite = !isFavorite;
+          });
+        },
+        child: Icon(
+          isFavorite ? Icons.favorite : Icons.favorite_border_outlined
+        ),
+    )
     );
   }
 }
