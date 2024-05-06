5b99e552e1c9e788bc10c2b2633f25462650d319
221110278 <165923810+221110278@users.noreply.github.com>
2024-05-07 02:31:54 +0700

    Update home

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tugas/components/allFilm.dart';
 import 'package:tugas/components/bottom_nav_bar.dart';
 import 'package:tugas/data/listFilm.dart';
 import 'package:tugas/screens/detail.dart';
@@ -6,121 +7,71 @@ import 'package:tugas/screens/detail.dart';
 class Home extends StatelessWidget {
   final String username;
   final List<int> selectedSeats;
-
   Home({Key? key, required this.username, required this.selectedSeats});
+  
+  get jumlahTiket => 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(
-          'Capt Cinema',
+        title:const Text(
+          'Capt Cinema', 
           style: TextStyle(
-            fontSize: 25,
+            fontSize: 25, 
             fontWeight: FontWeight.bold,
             color: Colors.white,
+            ),
           ),
-        ),
-        backgroundColor: Colors.blue,
+          backgroundColor: Colors.blue,
         centerTitle: true,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+
+            },
+            ),
+            IconButton(
+              icon: Icon(Icons.more_vert),
+              onPressed: () {
+                
+              },
+            )
+        ],
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+     body: Column(
+  crossAxisAlignment: CrossAxisAlignment.start,
+  children: [
+    Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
+          Text(
+            '',
+            style: TextStyle(
+              fontSize: 40,
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(8),
+            decoration: BoxDecoration(
+              color: Colors.black12,
+              borderRadius: BorderRadius.circular(8),
+            ),
             child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
+                Icon(
+                  Icons.person,
+                  color: Colors.black87,
+                ),
+                SizedBox(width: 6),
                 Text(
-                  '',
+                  '$username',
                   style: TextStyle(
-                    fontSize: 40,
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.all(8),
-                  decoration: BoxDecoration(
-                    color: Colors.black12,
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  child: Row(
-                    children: [
-                      Icon(
-                        Icons.person,
-                        color: Colors.black87,
-                      ),
-                      SizedBox(width: 6),
-                      Text(
-                        '$username',
-                        style: TextStyle(
-                          fontSize: 15,
-                          color: Colors.black87,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 8.0),
-            child: Text(
-              "Now Showing...",
-              style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-          Expanded(
-            child: ListView(
-              scrollDirection: Axis.horizontal,
-              children: [
-                Padding(
-                  padding: EdgeInsets.all(50),
-                  child: Row(
-                    children: [
-                      ...listFilm.map((e) {
-                        return Padding(
-                          padding: const EdgeInsets.all(25),
-                          child: Column(
-                            children: [
-                              Expanded(
-                                child: Image.network(
-                                  fit: BoxFit.cover,
-                                  '${e['gambar']}',
-                                ),
-                              ),
-                              SizedBox(height: 20),
-                              Center(
-                                child: SizedBox(
-                                  width: 220,
-                                  child: ElevatedButton(
-                                    onPressed: () {
-                                      Navigator.of(context).push(
-                                        MaterialPageRoute(
-                                          builder: (context) => Detail(nama: '${e["nama"]}'),
-                                        ),
-                                      );
-                                    },
-                                    child: Text(
-                                      '${e['nama']}',
-                                      style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-                                      textAlign: TextAlign.center,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              SizedBox(height: 8),
-                              Text('${e['genre']}'),
-                              SizedBox(height: 8),
-                            ],
-                          ),
-                        );
-                      }),
-                    ],
+                    fontSize: 15,
+                    color: Colors.black87,
                   ),
                 ),
               ],
@@ -128,7 +79,112 @@ class Home extends StatelessWidget {
           ),
         ],
       ),
-      bottomNavigationBar: MyBottomNavigationBar(
+      
+    ),
+    Padding(
+      padding: const EdgeInsets.only(left: 8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            "Now Showing...",
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold
+            )
+          ),
+          SizedBox(
+            width: 30,
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => AllFilm()),
+            );
+            },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.transparent,
+                elevation: 0,
+              ),
+            child: Row(
+              children: [
+                Text(
+                  "Lihat Semua",
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey
+                  ),
+                ),
+                Icon(Icons.arrow_right),
+              ],
+            ),
+          ),
+
+        ],
+      ),
+      ),
+      
+      Expanded(
+        child: 
+        ListView(
+        scrollDirection: Axis.horizontal,
+        children:[Padding(
+          padding: EdgeInsets.all(50),
+          child: Row(
+            children: [
+              ...listFilm.map((e) {
+                return Padding(
+                  padding: const EdgeInsets.all(25),
+                  child: Column(
+                      children: [
+                        Expanded(
+                          child: Image.network(
+                            fit: BoxFit.cover,
+                            '${e['gambar']}',
+                            ),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Center(
+                          child: SizedBox(
+                            width: 220,
+                            child: ElevatedButton(
+                              onPressed: () {
+                                Navigator.of(context)
+                                .push(MaterialPageRoute(builder: (context) {
+                                return Detail(nama: '${e["nama"]}',);}));
+                              },
+                              child: Text(
+                                '${e['nama']}',
+                                style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold),
+                                textAlign: TextAlign.center,
+                                ),
+                            ),
+                          ),
+                        ),
+                          SizedBox(
+                          height: 8,
+                        ),
+                        Text('${e['genre']}'),
+                        SizedBox(
+                          height: 8,
+                        ),
+                      ],
+                    ),
+                );
+              })
+            ],
+          ),
+          ),
+        ],
+      )
+      )
+  ]
+    ),
+    bottomNavigationBar: MyBottomNavigationBar(
         currentIndex: 0, // Set the current index
         onTap: (index) {
           // Handle navigation based on index
@@ -136,6 +192,9 @@ class Home extends StatelessWidget {
         bookedSeats: selectedSeats,
         username : "${username}", // Pass the list of selected seats
       ),
+
     );
   }
 }
+
+                 
