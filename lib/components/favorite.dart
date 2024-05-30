ecd6380d31c0a03252340f039f0131a7cb03a425
vincentlee2004 <165283616+vincentlee2004@users.noreply.github.com>
2024-05-30 14:10:41 +0700

    Menambahkan provider dan list film yang ditambahkan ke favorit

--- lib/components/favorite.dart
+++ lib/components/favorite.dart
@@ -1,30 +1,74 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:tugas/components/favoriteprovider.dart';
+import 'package:tugas/data/listFilm.dart';
 
-class Favorite extends StatefulWidget {
-  const Favorite({Key? key, required this.favoriteMovies});
+class Favorite extends StatelessWidget {
+  const Favorite({Key? key, required this.favoriteMovies}) : super(key: key);
+
   final List<String> favoriteMovies;
-  @override
-  State<Favorite> createState() => _FavoriteState();
-}
 
-class _FavoriteState extends State<Favorite> {
-  @override 
+  @override
   Widget build(BuildContext context) {
+    final favoriteProvider = Provider.of<FavoriteProvider>(context);
     return Scaffold(
       appBar: AppBar(
-        title: Text("Favorite Movie"),
+        title: const Text("Favorite Movies"),
       ),
       body: Padding(
-        padding: EdgeInsets.all(0),
+        padding: const EdgeInsets.all(0),
         child: ListView.builder(
-          itemCount: widget.favoriteMovies.length,
-          itemBuilder: (context, index){
-            return ListTile(
-              title: Text(widget.favoriteMovies[index]),
+          itemCount: favoriteProvider.favoriteMovies.length,
+          itemBuilder: (context, index) {
+            String favoriteMovieName = favoriteProvider.favoriteMovies[index];
+            Map<String, dynamic> favoriteMovie = listFilm.firstWhere(
+              (film) => film['nama'] == favoriteMovieName,
             );
-          }
-          ),
+
+            return Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  child: Row(
+                    children: [
+                      Image.network(
+                        favoriteMovie['gambar'],
+                        width: 100,
+                        height: 100,
+                        fit: BoxFit.cover,
+                      ),
+                      const SizedBox(width: 10),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              favoriteMovie['nama'],
+                              style: const TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            const SizedBox(height: 5),
+                            Text(favoriteMovie['genre']),
+                          ],
+                        ),
+                      ),
+                      IconButton(
+                        icon: const Icon(Icons.delete),
+                        onPressed: () {
+                          favoriteProvider.removeFavorite(favoriteMovieName);
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+                const Divider(),
+              ],
+            );
+          },
         ),
+      ),
     );
   }
 }
