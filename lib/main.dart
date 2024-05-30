67ae0f3f63b96a48248bf95a5a732280a07853bc
vincentlee2004 <165283616+vincentlee2004@users.noreply.github.com>
2024-05-30 13:19:53 +0700

    Menambah changenotifierprovider untuk favorite provider

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:tugas/components/login.dart';
 import 'package:tugas/components/prov_set.dart';
+import 'package:tugas/components/favoriteprovider.dart';
 
 void main() {
-  runApp(MultiProvider(providers: [
-    ChangeNotifierProvider(create: (_)=> SettingsProvider()),
-  ],
-  child: const MyApp(),
-  ));
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => SettingsProvider()),
+        ChangeNotifierProvider(create: (_) => FavoriteProvider()),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -19,8 +24,8 @@ class MyApp extends StatelessWidget {
     final prov = Provider.of<SettingsProvider>(context);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: prov.enableDarkMode == true ? prov.dark : prov.light,
-      home: Login(),
+      theme: prov.enableDarkMode ? prov.dark : prov.light,
+      home: const Login(),
     );
   }
 }
