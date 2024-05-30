7541ac081c65788eddb45db031f1d42b341103d6
vincentlee2004 <165283616+vincentlee2004@users.noreply.github.com>
2024-05-30 13:26:05 +0700

    Membuat popupmenubutton dan menavigasi FAB Favorite ke favorite.dart

--- lib/screens/detail.dart
+++ lib/screens/detail.dart
@@ -1,36 +1,87 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:tugas/components/bookedTiket.dart';
+import 'package:tugas/components/favorite.dart';
+import 'package:tugas/components/favoriteprovider.dart';
 import 'package:tugas/components/pesan-tiket.dart';
 import 'package:tugas/data/listFilm.dart';
+import 'package:tugas/screens/home.dart';
 
 class Detail extends StatefulWidget {
-  const Detail({Key? key, required this.nama,});
+  const Detail({Key? key, required this.nama}) : super(key: key);
 
   final String nama;
+
   @override
   State<Detail> createState() => _DetailState();
 }
 
 class _DetailState extends State<Detail> {
-  bool isFavorite = false;
   @override
   Widget build(BuildContext context) {
-  Map<String, dynamic> selectedFilm = listFilm.firstWhere((film) => film['nama'] == widget.nama);
+    final favoriteProvider = Provider.of<FavoriteProvider>(context);
+    bool isFavorite = favoriteProvider.favoriteMovies.contains(widget.nama);
+
+    Map<String, dynamic> selectedFilm = listFilm.firstWhere((film) => film['nama'] == widget.nama);
+
     return Scaffold(
       appBar: AppBar(
         title: Text(selectedFilm['nama']),
         actions: [
           IconButton(
             icon: Icon(Icons.search),
-            onPressed: () {
-
-            },
-            ),
-            IconButton(
-              icon: Icon(Icons.more_vert),
-              onPressed: () {
-                
-              },
-            )
+            onPressed: () {},
+          ),
+          PopupMenuButton(
+            icon: Icon(Icons.more_vert),
+            itemBuilder: (BuildContext context) => <PopupMenuEntry>[
+              PopupMenuItem(
+                child: ListTile(
+                  leading: Icon(Icons.home),
+                  title: Text('Home'),
+                  onTap: () {
+                    Navigator.pop(context);
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => Home(username: 'username', selectedSeats: []), // Replace with actual data
+                      ),
+                    );
+                  },
+                ),
+              ),
+              PopupMenuItem(
+                child: ListTile(
+                  leading: Icon(Icons.shopping_cart),
+                  title: Text('Tickets'),
+                  onTap: () {
+                    Navigator.pop(context); 
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => BookedSeatsScreen(bookedSeats: []), // Replace with actual data
+                      ),
+                    );
+                  },
+                ),
+              ),
+              PopupMenuItem(
+                child: ListTile(
+                  leading: Icon(Icons.favorite),
+                  title: Text('Favorite'),
+                  onTap: () {
+                    Navigator.pop(context); 
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => Favorite(favoriteMovies: [],),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
         ],
       ),
       body: SingleChildScrollView(
@@ -39,61 +90,60 @@ class _DetailState extends State<Detail> {
             padding: const EdgeInsets.all(50),
             child: Column(
               children: [
-                Image.network(
-                  selectedFilm['gambar'],
-                ),
+                Image.network(selectedFilm['gambar']),
                 const SizedBox(height: 20),
                 Text(
-                  '${selectedFilm['nama']}',
+                  selectedFilm['nama'],
                   style: const TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
                 ),
                 const SizedBox(height: 10),
                 const Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                  Icon(
-                  Icons.star,
-                  color: Colors.yellow,
-                  size: 24,
+                    Icon(
+                      Icons.star,
+                      color: Colors.yellow,
+                      size: 24,
+                    ),
+                    Icon(
+                      Icons.star,
+                      color: Colors.yellow,
+                      size: 24,
+                    ),
+                    Icon(
+                      Icons.star,
+                      color: Colors.yellow,
+                      size: 24,
+                    ),
+                    Icon(
+                      Icons.star,
+                      color: Colors.yellow,
+                      size: 24,
+                    ),
+                    Icon(
+                      Icons.star,
+                      color: Colors.yellow,
+                      size: 24,
+                    ),
+                  ],
                 ),
-                Icon(
-                  Icons.star,
-                  color: Colors.yellow,
-                  size: 24,
-                ),
-                Icon(
-                  Icons.star,
-                  color: Colors.yellow,
-                  size: 24,
-                ),
-                Icon(
-                  Icons.star,
-                  color: Colors.yellow,
-                  size: 24,
-                ),
-                Icon(
-                  Icons.star,
-                  color: Colors.yellow,
-                  size: 24,
-                ),
-                ],),
                 const SizedBox(height: 10),
                 Center(
                   child: SizedBox(
                     width: 500,
                     child: Text(
-                      '${selectedFilm['deskripsi']}',
-                      style: const TextStyle(fontSize: 16),textAlign: TextAlign.justify,
+                      selectedFilm['deskripsi'],
+                      style: const TextStyle(fontSize: 16),
+                      textAlign: TextAlign.justify,
                     ),
                   ),
                 ),
-               const SizedBox(height: 20),
+                const SizedBox(height: 20),
                 ElevatedButton(
                   onPressed: () {
-                    Navigator.push(context, MaterialPageRoute(
-                      builder: (context) {
+                    Navigator.push(context, MaterialPageRoute(builder: (context) {
                       return Tiket(nama: widget.nama);
-                  }));
+                    }));
                   },
                   child: const Text('Pesan Tiket'),
                 ),
@@ -103,15 +153,17 @@ class _DetailState extends State<Detail> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){
+        onPressed: () {
           setState(() {
-            isFavorite = !isFavorite;
+            if (isFavorite) {
+              favoriteProvider.removeFavorite(widget.nama);
+            } else {
+              favoriteProvider.addFavorite(widget.nama);
+            }
           });
         },
-        child: Icon(
-          isFavorite ? Icons.favorite : Icons.favorite_border_outlined
-        ),
-    )
+        child: Icon(isFavorite ? Icons.favorite : Icons.favorite_border_outlined),
+      ),
     );
   }
 }
