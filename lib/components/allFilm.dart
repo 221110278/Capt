e8d1fd3bd636981d5c669f1cce2471c17d939785
221110278 <165923810+221110278@users.noreply.github.com>
2024-06-15 15:37:56 +0700

    menambahkan linear progress indicator, mengubah kode choicechip dan fab refresh

--- lib/components/allFilm.dart
+++ lib/components/allFilm.dart
@@ -6,7 +6,6 @@ import 'package:tugas/screens/detail.dart';
 import 'package:tugas/screens/home.dart';
 
 class AllFilm extends StatefulWidget {
-
   const AllFilm({Key? key}) : super(key: key);
 
   @override
@@ -14,36 +13,71 @@ class AllFilm extends StatefulWidget {
 }
 
 class _AllFilmState extends State<AllFilm> {
-  List <Map<String,dynamic>> pilihan = [
-    {"genre":"Animation","selected":false},
-    {"genre":"Fantasy","selected":false},
-    {"genre":"Horor","selected":false},
-    {"genre":"Drama","selected":false},
-    {"genre":"Comedy","selected":false},
-    {"genre":"Romance","selected":false},
-    {"genre":"Thriller","selected":false},
+  List<Map<String, dynamic>> pilihan = [
+    {"genre": "Animation", "selected": false},
+    {"genre": "Fantasy", "selected": false},
+    {"genre": "Horor", "selected": false},
+    {"genre": "Drama", "selected": false},
+    {"genre": "Comedy", "selected": false},
+    {"genre": "Romance", "selected": false},
+    {"genre": "Thriller", "selected": false},
   ];
+
+  bool isLoading = true;
+
+  String selectedGenre = "";
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 2), () {
+      setState(() {
+        isLoading = false;
+      });
+    });
+  }
+
+  void _refreshPage() {
+    setState(() {
+      isLoading = true;
+      pilihan.forEach((element) {
+        element['selected'] = false;
+      });
+    });
+    Future.delayed(Duration(seconds: 2), () {
+      setState(() {
+        isLoading = false;
+        selectedGenre = "";
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    List<Map<String, dynamic>> filteredList = listFilm;
+    if (selectedGenre.isNotEmpty) {
+      filteredList = listFilm.where((film) => film['genre'] == selectedGenre).toList();
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text(
           "Capt Film",
           style: TextStyle(
-            fontSize: 25, 
+            fontSize: 25,
             fontWeight: FontWeight.bold,
-            ),
           ),
+        ),
         backgroundColor: Colors.white,
         actions: [
           Tooltip(
-               message: 'Search',
-               child: IconButton(
-                 icon: Icon(Icons.search),
-                 onPressed: () {},
-               ),
-             ),
-            PopupMenuButton(
+            message: 'Search',
+            child: IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {},
+            ),
+          ),
+          PopupMenuButton(
             icon: Icon(Icons.more_vert),
             itemBuilder: (BuildContext context) => <PopupMenuEntry>[
               PopupMenuItem(
@@ -51,11 +85,14 @@ class _AllFilmState extends State<AllFilm> {
                   leading: Icon(Icons.home),
                   title: Text('Home'),
                   onTap: () {
-                    Navigator.pop(context); 
+                    Navigator.pop(context);
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                        builder: (context) => Home(username: 'username', selectedSeats: []), 
+                        builder: (context) => Home(
+                          username: 'username',
+                          selectedSeats: [],
+                        ),
                       ),
                     );
                   },
@@ -66,11 +103,11 @@ class _AllFilmState extends State<AllFilm> {
                   leading: Icon(Icons.shopping_cart),
                   title: Text('Tickets'),
                   onTap: () {
-                    Navigator.pop(context); 
+                    Navigator.pop(context);
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                        builder: (context) => BookedSeatsScreen(bookedSeats: []), 
+                        builder: (context) => BookedSeatsScreen(bookedSeats: []),
                       ),
                     );
                   },
@@ -81,11 +118,11 @@ class _AllFilmState extends State<AllFilm> {
                   leading: Icon(Icons.favorite),
                   title: Text('Favorite'),
                   onTap: () {
-                    Navigator.pop(context); 
+                    Navigator.pop(context);
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                        builder: (context) => Favorite(favoriteMovies: []), 
+                        builder: (context) => Favorite(favoriteMovies: []),
                       ),
                     );
                   },
@@ -95,94 +132,110 @@ class _AllFilmState extends State<AllFilm> {
           ),
         ],
       ),
-      body: Padding(
-        padding: EdgeInsets.all(0),
-        child: Column(
-          children: [
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
+      body: isLoading
+          ? LinearProgressIndicator()
+          : Padding(
+              padding: EdgeInsets.all(8),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  for (int i = 0; i < pilihan.length; i++)
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 7),
-                child: ChoiceChip(
-                  label: Text("${pilihan[i]['genre']}"),
-                  selected: pilihan[i]['selected'],
-                  onSelected: (bool e) {
-                    setState(() {
-                      pilihan[i]['selected'] = e;
-                    });
-                  },
-                ),
-              ),
-          ],
-        ),
-      ),
-            Expanded(
-              child: GridView(gridDelegate: 
-              SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                mainAxisSpacing: 8,
-                crossAxisSpacing: 8
-                ),
-                children: [
-                  ...listFilm.map((e){
-                    return Card(
-                      elevation: 4,
-                      child: Padding(
-                        padding: const EdgeInsets.all(0),
-                        child: Column(
-                          children: [
-                            Expanded(
-                              child: Image.network(
-                                fit: BoxFit.cover,
-                                '${e['gambar']}',
-                              ),
-                            ),
-                            SizedBox(
-                                height: 20,
-                              ),
-                            SizedBox(
-                              width: double.infinity,
-                              child: ElevatedButton(onPressed: (){
-                                Navigator.of(context)
-                                  .push(MaterialPageRoute(builder: (context) {
-                                  return Detail(nama: '${e["nama"]}');}));
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: [
+                        for (int i = 0; i < pilihan.length; i++)
+                          Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 7, vertical: 10),
+                            child: ChoiceChip(
+                              label: Text("${pilihan[i]['genre']}"),
+                              selected: pilihan[i]['selected'],
+                              onSelected: (bool selected) {
+                                setState(() {
+                                  pilihan.forEach((e) {
+                                    e['selected'] = false;
+                                  });
+                                  pilihan[i]['selected'] = selected;
+                                  selectedGenre = selected ? pilihan[i]['genre'] : "";
+                                });
                               },
-                              style: ElevatedButton.styleFrom(
-                                backgroundColor: Colors.transparent,
-                                elevation: 0
-                              ), 
-                              child: Text(
-                                '${e['nama']}',
-                                style: TextStyle(
-                                  fontSize: 15,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.black
+                            ),
+                          ),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: GridView(
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        mainAxisSpacing: 8,
+                        crossAxisSpacing: 8,
+                      ),
+                      children: [
+                        ...filteredList.map((e) {
+                          return Card(
+                            elevation: 4,
+                            child: Padding(
+                              padding: const EdgeInsets.all(0),
+                              child: Column(
+                                children: [
+                                  Expanded(
+                                    child: Image.network(
+                                      '${e['gambar']}',
+                                      fit: BoxFit.cover,
+                                    ),
                                   ),
-                                textAlign: TextAlign.center,
-                                ),
+                                  SizedBox(
+                                    height: 20,
+                                  ),
+                                  SizedBox(
+                                    width: double.infinity,
+                                    child: ElevatedButton(
+                                      onPressed: () {
+                                        Navigator.of(context).push(
+                                          MaterialPageRoute(
+                                            builder: (context) =>
+                                                Detail(nama: '${e["nama"]}'),
+                                          ),
+                                        );
+                                      },
+                                      style: ElevatedButton.styleFrom(
+                                        backgroundColor: Colors.transparent,
+                                        elevation: 0,
+                                      ),
+                                      child: Text(
+                                        '${e['nama']}',
+                                        style: TextStyle(
+                                          fontSize: 15,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.black,
+                                        ),
+                                        textAlign: TextAlign.center,
+                                      ),
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 8,
+                                  ),
+                                  Text('${e['genre']}'),
+                                  SizedBox(
+                                    height: 8,
+                                  ),
+                                ],
                               ),
                             ),
-                            SizedBox(
-                                height: 8,
-                              ),
-                              Text('${e['genre']}'),
-                              SizedBox(
-                                height: 8,
-                              ),
-                          ],
-                        ),
-                        ),
-                    );
-                  })
+                          );
+                        })
+                      ],
+                    ),
+                  ),
                 ],
               ),
             ),
-          ],
-        ),
-        ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _refreshPage,
+        tooltip: 'Refresh',
+        child: Icon(Icons.refresh),
+      ),
     );
   }
 }
