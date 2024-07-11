dfa9e8eaec15139a7facd9d5a374013fe0cc799e
JohnAlexanderSalim <165993587+JohnAlexanderSalim@users.noreply.github.com>
2024-07-11 22:51:21 +0700

    menambahkan fitur hari dan jam pemesanan tiket

--- lib/components/pesan-tiket.dart
+++ lib/components/pesan-tiket.dart
@@ -15,34 +15,57 @@ class Tiket extends StatefulWidget {
 }
 
 class _TiketState extends State<Tiket> {
+  late Map<String, dynamic> selectedFilm;
+  late int jumlahTiket;
+  
+
+  final List<DateTime> dateOptions = [
+    DateTime.now(),
+    DateTime.now().add(Duration(days: 1)),
+    DateTime.now().add(Duration(days: 2)),
+    DateTime.now().add(Duration(days: 3)),
+  ];
+
+  final List<TimeOfDay> timeOptions = [
+    TimeOfDay(hour: 10, minute: 0),
+    TimeOfDay(hour: 12, minute: 0),
+    TimeOfDay(hour: 14, minute: 0),
+    TimeOfDay(hour: 16, minute: 0),
+  ];
+
+  DateTime? selectedDate;
+  TimeOfDay? selectedTime;
+
+  @override
+  void initState() {
+    super.initState();
+    selectedFilm = listFilm.firstWhere((film) => film['nama'] == widget.nama);
+    jumlahTiket = selectedFilm['jumlahTiket'];
+  }
+
   @override
   Widget build(BuildContext context) {
-    Map<String, dynamic> selectedFilm =
-        listFilm.firstWhere((film) => film['nama'] == widget.nama);
-
-    int jumlahTiket = selectedFilm['jumlahTiket'];
-
     return Scaffold(
       appBar: AppBar(
-        title:const Text(
-          'Pemesanan Tiket', 
+        title: const Text(
+          'Pemesanan Tiket',
           style: TextStyle(
-            fontSize: 25, 
+            fontSize: 25,
             fontWeight: FontWeight.bold,
             color: Colors.white,
-            ),
           ),
-          backgroundColor: Colors.blue,
+        ),
+        backgroundColor: Colors.blue,
         centerTitle: true,
         actions: [
           Tooltip(
-               message: 'Search',
-               child: IconButton(
-                 icon: Icon(Icons.search),
-                 onPressed: () {},
-               ),
-             ),
-             PopupMenuButton(
+            message: 'Search',
+            child: IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {},
+            ),
+          ),
+          PopupMenuButton(
             icon: Icon(Icons.more_vert),
             itemBuilder: (BuildContext context) => <PopupMenuEntry>[
               PopupMenuItem(
@@ -50,7 +73,7 @@ class _TiketState extends State<Tiket> {
                   leading: Icon(Icons.home),
                   title: Text('Home'),
                   onTap: () {
-                    Navigator.pop(context); 
+                    Navigator.pop(context);
                     Navigator.push(
                       context,
                       MaterialPageRoute(
@@ -65,11 +88,11 @@ class _TiketState extends State<Tiket> {
                   leading: Icon(Icons.shopping_cart),
                   title: Text('Tickets'),
                   onTap: () {
-                    Navigator.pop(context); 
+                    Navigator.pop(context);
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                        builder: (context) => BookedSeatsScreen(bookedSeats: []), 
+                        builder: (context) => BookedSeatsScreen(bookedSeats: []),
                       ),
                     );
                   },
@@ -80,11 +103,11 @@ class _TiketState extends State<Tiket> {
                   leading: Icon(Icons.favorite),
                   title: Text('Favorite'),
                   onTap: () {
-                    Navigator.pop(context); 
+                    Navigator.pop(context);
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                        builder: (context) => Favorite(favoriteMovies: []), 
+                        builder: (context) => Favorite(favoriteMovies: []),
                       ),
                     );
                   },
@@ -128,10 +151,7 @@ class _TiketState extends State<Tiket> {
                       child: IconButton(
                         onPressed: () {
                           setState(() {
-                            if (jumlahTiket > 0) {
-                              jumlahTiket--;
-                              selectedFilm['jumlahTiket'] = jumlahTiket; 
-                            }
+                            if (jumlahTiket > 0) jumlahTiket--;
                           });
                         },
                         icon: Icon(Icons.remove),
@@ -150,26 +170,79 @@ class _TiketState extends State<Tiket> {
                         onPressed: () {
                           setState(() {
                             jumlahTiket++;
-                            selectedFilm['jumlahTiket'] = jumlahTiket; 
+                            selectedFilm['jumlahTiket'] = jumlahTiket;
                           });
                         },
                         icon: Icon(Icons.add),
                       ),
                     ),
                   ],
+                ),
+                SizedBox(height: 20),
+                Text(
+                  "Pilih Tanggal",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
                   ),
-
-
+                ),
+                Wrap(
+                  spacing: 10,
+                  children: dateOptions.map((date) {
+                    return ChoiceChip(
+                      label: Text(
+                        '${date.day}-${date.month}-${date.year}',
+                      ),
+                      selected: selectedDate == date,
+                      onSelected: (bool selected) {
+                        setState(() {
+                          selectedDate = selected ? date : null;
+                        });
+                      },
+                    );
+                  }).toList(),
+                ),
+                SizedBox(height: 20),
+                Text(
+                  "Pilih Waktu",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Wrap(
+                  spacing: 10,
+                  children: timeOptions.map((time) {
+                    return ChoiceChip(
+                      label: Text(
+                        '${time.format(context)}',
+                      ),
+                      selected: selectedTime == time,
+                      onSelected: (bool selected) {
+                        setState(() {
+                          selectedTime = selected ? time : null;
+                        });
+                      },
+                    );
+                  }).toList(),
+                ),
                 SizedBox(height: 20),
                 ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => ListTiket(jumlahTiket: jumlahTiket),
-                      ),
-                    );
-                  },
+                  onPressed: selectedDate != null && selectedTime != null
+                      ? () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => ListTiket(
+                                jumlahTiket: jumlahTiket,
+                                selectedDate: selectedDate!,
+                                selectedTime: selectedTime!,
+                              ),
+                            ),
+                          );
+                          
+                        }
+                      : null,
                   child: Text('Lihat Tiket'),
                 ),
               ],
